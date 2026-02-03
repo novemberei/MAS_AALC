54d9def98a486a1a16856b18e72515d5e6619c7c
淼九清 <ll13400709119@163.com>
2026-02-03 10:45:38 +0800

    feat: 上传本体
    仓库使用的本体

---
+++ delete_user_log.bat
@@ -0,0 +1,24 @@
+@echo off
+rem 此脚本运行时会删除同级目录下的user.log
+cd /d "%～dp0"
+
+set "LOGFILE=user.log"
+echo wantdelete: "%LOGFILE%"
+
+if not exist "%LOGFILE%" (
+    echo no
+    timeout /t 3 /nobreak >nul
+    exit /b
+)
+
+echo yes
+del /f /q "%LOGFILE%" >nul 2>&1
+
+if exist "%LOGFILE%" (
+    echo oshift
+) else (
+    echo oyes
+)
+
+timeout /t 3 /nobreak >nul
+exit
